970234de81e046c896190fa2b424d54e5f21383f
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2025-07-01 03:44:08 +0300

    q594 solution added

---
+++ More Problem Solutions/594_longest_harmonious_subsequence.py
@@ -0,0 +1,58 @@
+'''
+We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2,2,5,2,3,7]
+
+Output: 5
+
+Explanation:
+
+The longest harmonious subsequence is [3,2,2,2,3].
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: 2
+
+Explanation:
+
+The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+
+Example 3:
+
+Input: nums = [1,1,1,1]
+
+Output: 0
+
+Explanation:
+
+No harmonic subsequence exists.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-109 <= nums[i] <= 109
+'''
+
+
+from collections import Counter
+
+class Solution:
+    def findLHS(self, nums):
+        counter = Counter(nums)
+        max_len = 0
+
+        for num in counter:
+            if num + 1 in counter:
+                max_len = max(max_len, counter[num] + counter[num + 1])
+
+        return max_len
