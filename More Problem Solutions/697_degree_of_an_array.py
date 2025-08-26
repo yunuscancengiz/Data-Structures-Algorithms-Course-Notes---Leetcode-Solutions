599831c99ee4da9f3543f2e769fd9fc63ed2ef4f
Yunus Can Cengiz <yunuscancengiz@Yunus-MacBook-Pro.local>
2025-08-26 13:40:40 +0300

    q697 solution added

---
+++ More Problem Solutions/697_degree_of_an_array.py
@@ -0,0 +1,50 @@
+'''
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,1]
+Output: 2
+Explanation: 
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+Example 2:
+
+Input: nums = [1,2,2,3,1,4,2]
+Output: 6
+Explanation: 
+The degree is 3 because the element 2 is repeated 3 times.
+So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6.
+ 
+
+Constraints:
+
+nums.length will be between 1 and 50,000.
+nums[i] will be an integer between 0 and 49,999.
+
+'''
+
+
+from typing import List
+
+
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        freq_map = {}
+        for idx, num in enumerate(nums):
+            if num not in freq_map:
+                freq_map[num] = []
+            freq_map[num].append(idx)
+
+        max_ = max(len(idxs) for idxs in freq_map.values())
+        result = float('inf')
+        for idxs in freq_map.values():
+            if len(idxs) == max_:
+                result = min(result, idxs[-1] - idxs[0] + 1)
+        return result
